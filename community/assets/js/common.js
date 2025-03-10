21a70815c135e90abdd22f85cb4552fd2f4de5f4
junsik <deeptree97@gmail.com>
2025-03-11 00:00:06 +0900

    feat: 좋아요수 등 숫자 포매팅 함수

--- community/assets/js/common.js
+++ community/assets/js/common.js
@@ -7,3 +7,10 @@ export function hideHelperText(helper) {
   helper.textContent = "";
   helper.style.display = "none";
 }
+
+export function formatNumber(num) {
+  if (num >= 100000) return (num / 1000).toFixed(0) + "k";
+  if (num >= 10000) return (num / 1000).toFixed(0) + "k";
+  if (num >= 1000) return (num / 1000).toFixed(1) + "k";
+  return num;
+}
